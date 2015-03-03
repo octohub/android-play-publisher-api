c509ec4459df67155ca3cd8e9feab765042c877e
Richard Lucas <richard.lucas11@gmail.com>
2015-03-03 16:53:47 -0500

    Improved scheduler class.

--- v2/java/src/com/richardlucasapps/scheduler/Scheduler.java
+++ v2/java/src/com/richardlucasapps/scheduler/Scheduler.java
@@ -2,15 +2,10 @@ package com.richardlucasapps.scheduler;
 
 import com.google.play.developerapi.samples.UploadApkWithListing;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -20,64 +15,65 @@ import java.util.concurrent.TimeUnit;
  */
 public class Scheduler {
 
-    private static int SECONDS_IN_A_DAY = 86400;
+    private static int SECONDS_TO_REPEAT = 86400; //86400 is the number of seconds in one day
 
     public static void main(String[] args){
 
         ZonedDateTime utcNow = ZonedDateTime.now(ZoneId.of("Z"));
-        ZonedDateTime utcStart = utcNow.withHour(0).withMinute(0).withSecond(0);
 
-        utcStart = utcStart.plusDays(1);
+        ZonedDateTime utcStartUploadAPK = utcNow.withHour(0).withMinute(0).withSecond(0);
+        utcStartUploadAPK = utcStartUploadAPK.plusDays(1);
+        ZonedDateTime utcStartBuildAPK = utcNow.withHour(12).withMinute(0).withSecond(0);
+        utcStartBuildAPK = utcStartBuildAPK.plusDays(1);
 
-        Duration duration = Duration.between(utcNow, utcStart);
-        long initialDelay = duration.getSeconds();
+        Duration durationUploadAPK = Duration.between(utcNow, utcStartUploadAPK);
+        long uploadApkInitialDelay = durationUploadAPK.getSeconds();
 
+        Duration duration = Duration.between(utcNow, utcStartBuildAPK);
+        long buildApkInitialDelay = duration.getSeconds();
 
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(new MyRunnableTask(), initialDelay,
-                SECONDS_IN_A_DAY, TimeUnit.SECONDS);
-        System.out.println(initialDelay);
+        System.out.println("UploadApk Initial Delay: " + String.valueOf(uploadApkInitialDelay));
+        System.out.println("BuildApk Initial Delay: " + String.valueOf(buildApkInitialDelay));
 
+        new BuildApk().run(); //calling run directly will not start new thread, do this so we know we have a fresh apk
+                              //ready for upload.
+
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new UploadApk(), uploadApkInitialDelay,
+                SECONDS_TO_REPEAT, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(new BuildApk(), buildApkInitialDelay,
+                SECONDS_TO_REPEAT, TimeUnit.SECONDS);
     }
 
-    private static class MyRunnableTask implements Runnable {
+    private static class UploadApk implements Runnable {
         @Override
         public void run() {
             ZonedDateTime utcNow = ZonedDateTime.now(ZoneId.of("Z"));
-            System.out.println("running");
-            String versionCode = "";
+            System.out.println("Uploading APK, Current Time: " + utcNow.toString());
+            String[] array = new String[]{utcNow.toString()};
+            UploadApkWithListing.main(array);
+        }
+    }
+
+    private static class BuildApk implements Runnable {
+        @Override
+        public void run() {
+            ZonedDateTime utcNow = ZonedDateTime.now(ZoneId.of("Z"));
+            System.out.println("Building APK, Current Time: " + utcNow.toString());
+
+            String[] cmdAndArgs = {"cmd", "/c","build.bat"};
+            File dir = new File("C:/Users/Richard/Desktop/");
+
+            ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+            pb.directory(dir);
             try {
-                versionCode = getPropValues();
+                Process p = pb.start();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            String[] array = new String[]{utcNow.toString()};
-            UploadApkWithListing.main(array);
-
+            
         }
     }
 
-    private static String getPropValues() throws IOException {
 
-        String result = "";
-        Properties prop = new Properties();
-        String propFileName = "version.properties";
-
-        //InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-        InputStream inputStream = new FileInputStream("/Users/richard/Google Drive/Dev/updayte/app/version.properties");
-
-        if (inputStream != null) {
-            prop.load(inputStream);
-        } else {
-            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-        }
-
-        Date time = new Date(System.currentTimeMillis());
-
-        // get the property value and print it out
-        String versionCode = prop.getProperty("VERSION_CODE");
-
-        System.out.println(versionCode);
-        return result;
-    }
 }
